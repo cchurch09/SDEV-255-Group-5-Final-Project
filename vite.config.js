0874958000765cea8708edb441801ffc1ac35e8f
eryial-a <kyletn22@gmail.com>
2025-09-29 17:04:43 -0500

    fix config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://cchurch09.github.io/SDEV-255-Group-5-Final-Project/",
+  base: "/SDEV-255-Group-5-Final-Project",
 })
