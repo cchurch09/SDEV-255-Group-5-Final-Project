5e90b9343b5a0247b482635291ac5f358dacb67c
cchurh09 <cchurch24@ivytech.edu>
2025-10-04 23:18:21 -0400

    adding changes to home, schedule and adding css

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { Button } from '../resources/Boiler'
-
+import { NavLink } from 'react-router-dom';
+import { Button } from '../resources/Boiler';
+import campus1 from '../photos/campus1.jpg';
+import office1 from '../photos/office1.jpg';
+import professor1 from '../photos/professor1.jpg';
 
 export default function Home() {
     return (
         <div className="text-center py-5">
-            <h1 className="display-4 fw-bold">Course Builder</h1>
-            <p>Welcome to the Course Builder! Click either button below to head to the right direction.</p>
+            <h1 className="display-4 fw-bold">Class Map</h1>
+            <h3 class name ="mt-5"></h3>
+                <div className="scroll-gallery">
+                    <img src={campus1} alt="Gallery 1" />
+                    <img src={office1} alt="Gallery 2" />
+                    <img src={professor1} alt="Gallery 3" />
+                </div>
+            <p>Welcome to Class Map! A place for students and teachers to build their schedules.</p>
             <div className="mt-4 d-flex flex-column align-items-center gap-3">
                 <NavLink to="/signup" className="btn btn-primary btn-lg"><Button text="Log In" classes="purplebutton" /></NavLink>
                 <NavLink to="/schedule" className="btn btn-primary btn-lg"><Button text="View Schedule" classes="graybutton" /></NavLink>
@@ -15,4 +23,5 @@ export default function Home() {
         </div>
     )
 }
+
 
--- src/pages/schedule.jsx
+++ src/pages/schedule.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Button } from '../resources/Boiler';
+import computerclass from '../photos/computer class.jpg';
+import scienceclass from '../photos/science class.jpg';
+import mathclass from '../photos/math class.jpg';
+
 
 export default function Schedule(){
 const courses = [
-    {id: 1, title: 'SDEV255',  description: 'This is a computer course'},
-    {id: 2, title: 'SCI202', description: 'This is a science course'},
-    {id: 3, title: 'MATH200', description: 'This is an math course'},
+    {id: 1, title: 'SDEV255',  description: 'Students will learn how to use and apply client and server-side scripting and application programming interfaces to build web-based applications which interact with a data source including XML and JSON. Students will prepare both front and back end content using techniques including, Hyper Text Markup Language and JavaScript to create dynamic data-driven web interfaces.', image: computerclass },
+    {id: 2, title: 'SCI202', description: 'An introductory course that includes the science of chemistry and measurement, atomic theory and the periodic table, chemical bonding, equation writing and balancing, stoichiometry, gases and acids/bases. Includes lab', image: scienceclass},
+    {id: 3, title: 'MATH200', description: 'Reviews linear equations, inequalities, graphing, and factoring algebraic expressions. Concentrates on properties of integer and rational exponents, systems of linear equations, radicals, radical equations, quadratic equations, functions including their graphs, and applications', image: mathclass},
 ]
-
 
 return (
     <div className="py-4">
@@ -18,6 +21,7 @@ return (
                     <div className="card h-100 bg-dark text-light">
                         <div className="card-body">
                             <h4 className="card-title">{course.title} <Button text="View Details" classes="graybutton" /></h4>
+                            <img src={course.image} alt={`${course.title} class`}className="polaroid"/>
                             <p className="card-text">{course.description}</p>
                         </div>
                     </div>
