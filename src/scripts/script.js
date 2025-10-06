237ba105bd8860d3307c84a8bfcf67749a12c688
eryial-a <kyletn22@gmail.com>
2025-10-06 11:59:07 -0500

    add course manipulation functions and update css

---
+++ src/scripts/script.js
@@ -0,0 +1,47 @@
+const backendUrl = 'https://group-5-final-project-backend.onrender.com/api/courses/'
+
+export const courses = {
+  async fetchAll() {
+    try {
+      const response = await fetch(backendUrl)
+      const data = await response.json()
+      return data
+    } catch (error) {
+      console.log(error)
+    }
+  },
+
+  async create(courseData) {
+    const response = await fetch(backendUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(courseData),
+    })
+    if (response.ok) {
+      alert("Created Course")
+    }
+    return response.json()
+  },
+
+  async update(id, courseData) {
+    const response = await fetch(`${backendUrl}/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(courseData),
+    })
+    if (response.ok) {
+      alert("Updated Course")
+    }
+    return response.json()
+  },
+
+  async delete(id) {
+    const response = await fetch(`${backendUrl}/${id}`, { 
+      method: 'DELETE' 
+    })
+    if (response.ok) {
+      alert("Deleted Course")
+    }
+    return response.json()
+  }
+}
